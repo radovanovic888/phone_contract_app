41358c85fde36cf0f0dfc1163f2eb8dd8ec8b65e
Sasa Radovanovic <sasaradovanovic0123456789@gmail.com>
2026-02-23 17:17:51 -0500

    main() with switch(c)

--- main.c
+++ main.c
@@ -13,3 +13,59 @@ void display_contracts(Contract *contracts, int *count);
 void add_contract_form(Contract *c);
 int delete_contract_prompt();
 
+int main() {
+	if (init_db() != 0) {
+		return 1;
+	}
+
+	init_ui();
+
+	int choice = 0;
+	int highlight = 1;
+	int c;
+
+	while(1) {
+		draw_menu(highlight);
+		c = getch();
+
+		switch(c) {
+			case KEY_UP:
+				if (highlight == 1) highlight = 4;
+				else --highlight;
+				break;
+			case KEY_DOWN:
+				if (highlight == 4) highlight = 1;
+				else ++highlight;
+				break;
+			case 10:
+				choice = highligh;
+				break;
+			default:
+				break;
+		}
+
+		if (choice != 0) {
+			if (choice == 1) {
+				Contract *contracts;
+				int count;
+				if (get_contracts(&contracts, &count) == 0) {
+					display_contracts(contracts, count);
+					if (contracts) free contracts;
+				}
+			} else if (choice == 2) {
+				Contract new_c;
+				add_contract_form(&new_c);
+				insert_contract(&new_c);
+			} else if (choice == 3) {
+				int id = delete_contract_prompt();
+				if (id > 0) delete_contract(id);
+			} else if (choice == 4) {
+				break;
+			}
+			choice = 0; // Reset choice
+		}
+	}
+
+	cleanup_ui();
+	return 0;
+}
