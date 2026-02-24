26544546d172baa122f8ed730e59b109003f6b72
Sasa Radovanovic <sasaradovanovic0123456789@gmail.com>
2026-02-24 11:02:36 -0500

    Added insert_contract(Contract *c) into db_manager.c file

--- db_manager.c
+++ db_manager.c
@@ -38,3 +38,32 @@ int init_db() {
     sqlite3_close(db);
     return 0;
 }
+
+int insert_contract(Contract *c) {
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    int rc = sqlite3_open(DB_NAME, &db);
+
+    if (rc != SQLITE_OK) return 1;
+
+    const char *sql = "INSERT INTO contracts (phone_number, company, service_plan, start_date, end_date, monthly_bill) VALUES (?, ?, ?, ?, ?, ?);";
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+
+    if (rc == SQLITE_OK) {
+        sqlite3_bind_text(res, 1, c->phone_number, -1, SQLITE_STATIC);
+        sqlite3_bind_text(res, 2, c->company, -1, SQLITE_STATIC);
+        sqlite3_bind_text(res, 3, c->service_plan, -1, SQLITE_STATIC);
+        sqlite3_bind_text(res, 4, c->start_date, -1, SQLITE_STATIC);
+        sqlite3_bind_text(res, 5, c->end_date, -1, SQLITE_STATIC);
+        sqlite3_bind_double(res, 6, c->monthly_bill);
+    } else {
+        sqlite3_close(db);
+        return 1;
+    }
+
+    rc = sqlite3_step(res);
+    sqlite3_finalize(res);
+    sqlite3_close(db);
+
+    return (rc == SQLITE_DONE) ? 0 : 1;
+}
