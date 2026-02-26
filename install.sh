009d79040fbfb66749e4c53af440ebb1aca5e093
Sasa Radovanovic <sasaradovanovic0123456789@gmail.com>
2026-02-25 19:55:34 -0500

    Get the directory where the script is located, using SCRIPT_DIR

--- install.sh
+++ install.sh
@@ -13,16 +13,20 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "$(BASH_SOURCE[0])" )" &> /dev/null && pwd )"
+
 echo "Updating package list and installing dependencis..."
 apt-get update
 apt-get install -y build-essential libncurses-dev libsqlite3-dev sqlite3
 
 echo "Compiling the application..."
-make -C ~/phone_contract_app
+# Use the script's directory to find the Makefile
+make -C "$SCRIPT_DIR"
 
 echo "Installing the application..."
 mkdir -p $INSTALL_DIR
-cp ~/phone_contract_app/$APP_NAME $INSTALL_DIR
+cp "$SCRIPT_DIR/$APP_NAME" $INSTALL_DIR
 
 # Optional:
 
